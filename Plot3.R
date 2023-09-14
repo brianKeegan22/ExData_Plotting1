64185af9ca8873ea69749f6870b1adabd2c88392
Brian Keegan <102035991+brianKeegan22@users.noreply.github.com>
2023-09-14 12:03:26 +0100

    Add files via upload

--- Plot3.R
+++ Plot3.R
@@ -1,13 +1,17 @@
 file='household_power_consumption.txt'
-
+##file is in current dir
 
 hhpower <- read.table(file, header = TRUE, sep = ";", dec = ".")
+##storing data in hhpower variable
 
 tab1 <- subset(hhpower, format(as.Date(hhpower$Date, format = "%d/%m/%Y"),"%Y")=="2007")
+##tab1 contains subset of data for 2007
 
 tab2 <- subset(tab1, format(as.Date(tab1$Date, format = "%d/%m/%Y"),"%m")=="02")
+##tab2 contains subset of data for Feb 2007
 
 tab <- subset(tab2, format(as.Date(tab2$Date, format = "%d/%m/%Y"),"%d")%in% c('01', '02'))
+##tab has the final data set for 1st to 2nd Feb 2007
 
 png("Plot3.png", width=480, height=480)
 
--- Plot4.R
+++ Plot4.R
@@ -1,30 +1,39 @@
 file='household_power_consumption.txt'
-
+##file is in current dir
 
 hhpower <- read.table(file, header = TRUE, sep = ";", dec = ".")
+##storing data in hhpower variable
 
 tab1 <- subset(hhpower, format(as.Date(hhpower$Date, format = "%d/%m/%Y"),"%Y")=="2007")
+##tab1 contains subset of data for 2007
 
 tab2 <- subset(tab1, format(as.Date(tab1$Date, format = "%d/%m/%Y"),"%m")=="02")
+##tab2 contains subset of data for Feb 2007
 
 tab <- subset(tab2, format(as.Date(tab2$Date, format = "%d/%m/%Y"),"%d")%in% c('01', '02'))
+##tab has the final data set for 1st to 2nd Feb 2007
 
 png("Plot4.png", width=480, height=480)
 
-
+##creating panel with 4 graphs
 par(mfrow=c(2,2))
+
+##Gobal active power plot(Plot1)
 plot(tab$Global_active_power, type="l", xaxt = "n", ylab = "Global Active Power(kilowatts)")
 axis(1, at = c(0,1500,2900), labels = c("Thu", "Fri","Sat"))
 
+##Voltage plot(Plot2)
 plot(tab$Voltage, type="l", xaxt = "n", xlab = "datetime" ,ylab = "Voltage")
 axis(1, at = c(0,1500,2900), labels = c("Thu", "Fri","Sat"))
 
+##Sub metering plots(Plot3)
 with(tab, plot(tab$Sub_metering_1, type="l", xaxt = "n", ylab = "Energy sub metering"))
 with(tab, points(tab$Sub_metering_2, col="red", type="l"))
 with(tab, points(tab$Sub_metering_3, col="blue", type="l"))
 legend("topright", pch=1,col=c("black","red","blue"),legend=c("Sub_metering 1","Sub_metering 2","Sub_metering 3"))
 axis(1, at = c(0,1500,2900), labels = c("Thu", "Fri","Sat"))
 
+##Gobal reactive power plot(Plot4)
 plot(tab$Global_reactive_power, type="l", xaxt = "n", xlab = "datetime", ylab = "Global Reactive Power")
 axis(1, at = c(0,1500,2900), labels = c("Thu", "Fri","Sat"))
 
